df56112ec30aabc48274f20d28214ed6c2934ae2
florian.llimos <florian.llimos@digitanie.org>
2025-05-12 14:36:27 +0200

    feat: add hero banner and style

---
+++ component/hero-banner.php
@@ -0,0 +1,70 @@
+<section id="hero-banner" aria-label="Bannière de présentation de Webolys">
+  <div class="hero-banner-aura1 aura" aria-hidden="true"></div>
+  <div class="hero-banner-container-text">
+    <h2>
+      Donnez vie à vos projets numériques avec des 
+      <span class="color">solutions web</span> sur mesure, innovantes et performantes.
+    </h2>
+    <p class="text">
+      Chez <strong><span class="color">Webolys</span></strong>, nous créons des expériences web sur mesure qui allient innovation, design et technologie.
+    </p>
+    <p class="text mask-and-show">
+      Nous travaillons main dans la main avec vous pour développer des solutions digitales adaptées à vos enjeux spécifiques, 
+      tout en assurant une performance optimale et une évolutivité pour accompagner votre succès sur le long terme.
+    </p>
+    <div class="container" aria-hidden="true">
+      <span id="word"></span>
+    </div>
+    <a href="contact.php" class="btn-contact" title="Contactez-nous dès maintenant">Contact</a>
+  </div>
+  <div class="hero-banner-container-image">
+    <img src="media/mockup_home.webp" alt="Illustration d'un site web développé par Webolys">
+  </div>
+  <div class="hero-banner-container-bottom">
+    <a href="#qui-sommes-nous" title="Aller à la section À propos">
+      <img src="media/arrow-down-sign-to-navigate.png" alt="Flèche vers la section suivante">
+    </a>
+  </div>
+  <div class="hero-banner-container-link">
+    <ul class="ul-social-link" aria-label="Liens vers nos réseaux sociaux et moyens de contact">
+      <!-- <li>
+        <a href="https://www.instagram.com/f.lmss/?hl=fr"
+          aria-label="Mon profil Instagram"
+          title="Profil Instagram"
+          target="_blank" rel="noopener noreferrer">
+          <img src="media/instagram.png" id="instagram" alt="Icône Instagram" loading="lazy">
+        </a>
+      </li> -->
+      <!-- <li>
+        <a href="https://www.facebook.com/florian.llimos/"
+          aria-label="Mon profil Facebook"
+          title="Profil Facebook"
+          target="_blank" rel="noopener noreferrer">
+          <img src="media/facebook.png" id="facebook" alt="Icône Facebook" loading="lazy">
+        </a>
+      </li> -->
+      <li>
+        <a href="https://www.linkedin.com/company/webolys"
+          aria-label="Mon profil LinkedIn"
+          title="Profil LinkedIn"
+          target="_blank" rel="noopener noreferrer">
+          <img src="media/linkedin.png" id="linkedin" alt="Icône LinkedIn">
+        </a>
+      </li>
+      <li>
+        <a href="tel:+33622005584"
+          aria-label="Appeler le 06 22 00 55 84"
+          title="Lien téléphonique">
+          <img src="media/telephone.png" id="telephone" alt="Icône téléphone">
+        </a>
+      </li>
+      <li>
+        <a href="javascript:location='mailto:\u0063\u006f\u006e\u0074\u0061\u0063\u0074\u0040\u0077\u0065\u0062\u006f\u006c\u0079\u0073\u002e\u0066\u0072';void 0"
+          aria-label="Envoyer un e-mail à contact@webolys.fr"
+          title="Envoyer un e-mail">
+          <img src="media/mail.png" id="mail" alt="Icône e-mail">
+        </a>
+      </li>
+    </ul>
+  </div>
+</section>
