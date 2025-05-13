6bdfaa530907b6ebda2d6ea96f273c3803bb58e5
florian.llimos <florian.llimos@digitanie.org>
2025-05-13 16:32:37 +0200

    feat: add confidentiality page

---
+++ politique-de-confidentialite.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <script type="text/javascript" src="js/email.min.js" defer></script>
+  <script src="js/init.js" type="text/javascript" defer></script>
+  <script src="js/newsletter.js" defer></script>
+  <script src="js/header.js" defer></script>
+  <title>Webolys - Politique de confidentialité | Agence Web</title>
+</head>
+<body>
+  <?php
+    require_once("component/header.php");
+  ?>
+  <main class="container-rgpd">
+    <?php
+      require_once("component/background.php");
+    ?>
+    <div class="container-banner-page">
+      <div>
+        <div>
+          <h2 id="title-page-politique-de-confidentialite">Politique de confidentialité</h2>
+        </div>
+      </div>
+    </div>
+    <section class="privacy-policy" role="document" aria-labelledby="privacy-title">
+      <div class="container">
+        <p>Dernière mise à jour : <strong>08 avril 2025</strong></p>
+        <h3>1. Qui sommes-nous ?</h3>
+        <p>
+          Le site <strong>www.webolys.fr</strong> est édité par <strong>webolys</strong>, situé à <strong>Saint Paul de Jarrat (09000)</strong>.
+        </p>
+        <h3>2. Données personnelles collectées</h3>
+        <p>
+          Nous collectons vos données personnelles lorsque vous remplissez les formulaires suivants sur notre site :
+        </p>
+        <ul>
+          <li><strong>Formulaire de contact :</strong> nom, prénom, adresse e-mail, téléphone, message.</li>
+          <li><strong>Formulaire de newsletter :</strong> adresse e-mail uniquement.</li>
+        </ul>
+        <h3>3. Finalités de la collecte</h3>
+        <p>Les données collectées sont utilisées pour les finalités suivantes :</p>
+        <ul>
+          <li><strong>Formulaire de contact :</strong> pour répondre à vos demandes, questions, ou devis.</li>
+          <li><strong>Formulaire de newsletter :</strong> pour vous envoyer des informations ou promotions sur nos services.</li>
+        </ul>
+        <h3>4. Base légale du traitement</h3>
+        <p>Nous traitons vos données personnelles sur la base de votre consentement, notamment lorsque vous remplissez un formulaire de contact ou vous abonnez à notre newsletter.</p>
+        <h3>5. Durée de conservation des données</h3>
+        <p>
+          Nous conservons vos données personnelles pour une durée maximale de <strong>365 jours</strong> après votre dernier contact ou votre dernier abonnement à notre newsletter, sauf si une durée plus longue est nécessaire pour répondre à des obligations légales.
+        </p>
+        <h3>6. Stockage des données</h3>
+        <p>
+          Vos données sont stockées de manière sécurisée dans nos boite mail.
+        </p>
+        <h3>7. Vos droits</h3>
+        <p>Conformément à la législation en vigueur, vous disposez des droits suivants concernant vos données personnelles :</p>
+        <ul>
+          <li><strong>Accès :</strong> Vous pouvez demander l'accès aux données que nous stockons à votre sujet.</li>
+          <li><strong>Rectification :</strong> Vous pouvez demander la correction de toute donnée inexacte ou incomplète.</li>
+          <li><strong>Suppression :</strong> Vous pouvez demander la suppression de vos données personnelles à tout moment.</li>
+          <li><strong>Opposition :</strong> Vous pouvez vous opposer à l’utilisation de vos données à des fins de marketing direct.</li>
+          <li><strong>Portabilité :</strong> Vous pouvez demander la portabilité de vos données sous un format structuré, couramment utilisé et lisible par machine.</li>
+        </ul>
+        <p>
+          Pour exercer vos droits, vous pouvez nous contacter à l’adresse suivante : <strong>contact@webolys.fr</strong> ou par courrier à l’adresse suivante : <strong>1 avenue du Scios, 09000 Saint Paul de Jarrat, France</strong>.
+        </p>
+        <h3>8. Sécurité des données</h3>
+        <p>
+          Nous mettons en œuvre des mesures de sécurité techniques et organisationnelles pour protéger vos données personnelles contre tout accès non autorisé, altération, divulgation ou destruction.
+        </p>
+        <h3>9. Partage des données</h3>
+        <p>
+          Vos données personnelles ne seront jamais revendues à des tiers.
+        </p>
+        <h3>10. Cookies</h3>
+        <p>
+          Notre site utilise des cookies uniquement pour la gestion de la session de navigation (cookies techniques) et pour améliorer l'expérience utilisateur. Aucune donnée personnelle n’est collectée via ces cookies.
+        </p>
+        <h3>11. Modification de la politique de confidentialité</h3>
+        <p>
+          Nous nous réservons le droit de modifier cette politique de confidentialité à tout moment. Toute modification sera publiée sur cette page avec la date de la dernière mise à jour.
+        </p>
+      </div>
+    </section>
+  </main>
+  <?php
+    require_once("component/footer.php");
+  ?>
+</body>
+</html>
