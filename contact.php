db7e078a48b3a4fac09491b6cbf8f61c4eef3824
florian.llimos <florian.llimos@digitanie.org>
2025-05-13 14:45:39 +0200

    feat: add new page contact

---
+++ contact.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Webolys - Contact | Agence Web</title>
+</head>
+<body>
+  <?php
+    require_once("component/header.php");
+  ?>
+  <main>
+    <?php
+      require_once("component/background.php");
+    ?>
+  </main>
+  <?php
+    require_once("component/footer.php");
+  ?>
+</body>
+</html>
