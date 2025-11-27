4a277abe6d7a4bbcb5f44c8befc7975634a83baa
florianllimos <llimosflorian1@gmail.com>
2025-11-27 10:18:37 +0100

    fix: add effect on portfolio

--- component/portfolio.php
+++ component/portfolio.php
@@ -1,4 +1,21 @@
 <section class="container-portfolio" id="mes-realisations" aria-labelledby="portfolio-title">
+  <div class="portfolio-aura1"></div>
+  <div class="portfolio-aura2"></div>
+  <div class="portfolio-particles">
+    <div class="particle"></div>
+    <div class="particle"></div>
+    <div class="particle"></div>
+    <div class="particle"></div>
+    <div class="particle"></div>
+    <div class="particle"></div>
+    <div class="particle"></div>
+    <div class="particle"></div>
+    <div class="particle"></div>
+    <div class="particle"></div>
+    <div class="particle"></div>
+    <div class="particle"></div>
+  </div>
+  
   <span class="portfolio-badge">
     <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
       <circle cx="12" cy="12" r="3"></circle>
